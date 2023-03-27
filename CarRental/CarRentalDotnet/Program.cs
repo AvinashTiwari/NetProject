d4b00d9a22cbf2fab7dfa8e30646725cebf720c9
AvinashTiwari85 <qwe123kids@gmail.com>
2023-03-26 20:27:05 -0600

    no message

--- CarRental/CarRentalDotnet/Program.cs
+++ CarRental/CarRentalDotnet/Program.cs
@@ -16,7 +16,7 @@ namespace CarRentalDotnet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AddRentalRecord());
         }
     }
 }
